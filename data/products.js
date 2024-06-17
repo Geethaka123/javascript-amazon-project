ffb6363fa9ebf60d1dec7ae7164c6f11b2ba9910
Geethaka123 <geethakasandesh@gmail.com>
2024-06-17 15:53:25 +0530

    convert loadProducts from XHR to fetch

--- data/products.js
+++ data/products.js
@@ -73,6 +73,29 @@ export class Appliances extends Product {
 
 export let products = [];
 
+export function loadProductsFetch() {
+  const promise = fetch("https://supersimplebackend.dev/products")
+    .then((response) => {
+      return response.json();
+    })
+    .then((productData) => {
+      products = productData.map((productDetails) => {
+        if (productDetails.type === "clothing") {
+          return new Clothing(productDetails);
+        }
+        if (productDetails.type === "appliance") {
+          return new Appliances(productDetails);
+        }
+        return new Product(productDetails);
+      });
+      console.log("load products");
+    });
+  return promise;
+}
+// loadProductsFetch().then(() => {
+//   console.log("next step");
+// });
+
 export function loadProducts(fun) {
   const xhr = new XMLHttpRequest();
   xhr.addEventListener("load", () => {
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,17 +1,18 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
-import { loadProducts } from "../data/products.js";
+import { loadProducts, loadProductsFetch } from "../data/products.js";
 import { loadCart } from "../data/cart.js";
 // import "../data/car.js";
 // import "../data/cart-class.js";
 
 //let us run multiple promises at the same time
 Promise.all([
-  new Promise((resolve) => {
-    loadProducts(() => {
-      resolve("value1"); //go to next step
-    });
-  }),
+  //   new Promise((resolve) => {
+  //     loadProducts(() => {
+  //       resolve("value1"); //go to next step
+  //     });
+  //   }),
+  loadProductsFetch(),
   new Promise((resolve) => {
     loadCart(() => {
       resolve();
