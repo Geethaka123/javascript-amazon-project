6d7401d6416659ebfcd67b0646955fe81ff1ff90
Geethaka123 <geethakasandesh@gmail.com>
2024-06-17 15:33:56 +0530

    use promises to load the order page

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,9 +1,49 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { loadProducts } from "../data/products.js";
+import { loadCart } from "../data/cart.js";
 // import "../data/car.js";
 // import "../data/cart-class.js";
-loadProducts(() => {
+
+//let us run multiple promises at the same time
+Promise.all([
+  new Promise((resolve) => {
+    loadProducts(() => {
+      resolve("value1"); //go to next step
+    });
+  }),
+  new Promise((resolve) => {
+    loadCart(() => {
+      resolve();
+    });
+  }),
+]).then((values) => {
+  console.log(values);
   renderOrderSummary();
   renderPaymentSummary();
 });
+
+// new Promise((resolve) => {
+//   loadProducts(() => {
+//     resolve("value1"); //go to next step
+//   });
+// })
+//   .then((value) => {
+//     console.log(value);
+//     return new Promise((resolve) => {
+//       loadCart(() => {
+//         resolve();
+//       });
+//     });
+//   })
+//   .then(() => {
+//     renderOrderSummary();
+//     renderPaymentSummary();
+//   });
+
+// loadProducts(() => {
+//   loadCart(() => {
+//     renderOrderSummary();
+//     renderPaymentSummary();
+//   });
+// });
